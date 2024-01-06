32648c55582a6b4b50c92f816ce4b9ad81a19cad
Rayvant Durani <rayvant310@gmail.com>
2024-01-06 11:26:36 +0530

    Added Project to Git

---
+++ src/test/java/com/Secure/SecureKeyCloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Secure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureKeyCloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
